cc5744c8301498738f5e62e4da9ef80fa9be65be
이제훈 <2eejehoon@gmail.com>
2024-03-28 12:24:53 +0900

    단답형 질문 type assertion 추가

--- src/components/edit/ShortTypeQuestion/ShortTypeQuestion.tsx
+++ src/components/edit/ShortTypeQuestion/ShortTypeQuestion.tsx
@@ -1,4 +1,5 @@
 import { useAppSelector } from "../../../store";
+import { isShortQuestion } from "../../../type";
 import { StyledP } from "./ShortTypeQuestionStyle";
 
 interface ShortTypeQuestionProps {
@@ -6,7 +7,12 @@ interface ShortTypeQuestionProps {
 }
 
 function ShortTypeQuestion({ questionIndex }: ShortTypeQuestionProps) {
-  const text = useAppSelector((state) => state.survey.questions[questionIndex].text);
+  const text = useAppSelector((state) => {
+    const question = state.survey.questions[questionIndex];
+    if (isShortQuestion(question)) {
+      return question.text;
+    }
+  });
 
   return <StyledP>{text}</StyledP>;
 }
