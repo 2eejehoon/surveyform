a4ccee65a42f9be510414192d48cb73a6de4c072
이제훈 <2eejehoon@gmail.com>
2024-03-28 12:11:45 +0900

    Checkbox 질문 type assertion 추가

--- src/components/edit/CheckboxTypeQuestion/CheckboxTypeQuestion.tsx
+++ src/components/edit/CheckboxTypeQuestion/CheckboxTypeQuestion.tsx
@@ -3,6 +3,7 @@ import { useAppSelector } from "../../../store";
 import CheckboxOption from "../CheckboxOption/CheckboxOption";
 import AddOptionButton from "../AddOptionButton/AddOptionButton";
 import { useRef } from "react";
+import { isCheckboxQuestion } from "../../../type";
 
 interface CheckboxTypeQuestionProps {
   questionIndex: number;
@@ -12,12 +13,17 @@ function CheckboxTypeQuestion({ questionIndex }: CheckboxTypeQuestionProps) {
   const dragStartRef = useRef<number | null>(null);
   const dragEndRef = useRef<number | null>(null);
 
-  const options = useAppSelector((state) => state.survey.questions[questionIndex].options);
+  const options = useAppSelector((state) => {
+    const question = state.survey.questions[questionIndex];
+    if (isCheckboxQuestion(question)) {
+      return question.options;
+    }
+  });
 
   return (
     <>
       <StyledList>
-        {options.map((_, optionIndex) => {
+        {(options ?? []).map((_, optionIndex) => {
           const key = `${questionIndex}-${optionIndex}`;
           return (
             <CheckboxOption
